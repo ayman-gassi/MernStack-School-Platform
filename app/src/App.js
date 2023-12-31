dfdc338bb41039b395aea58efdb3d890cf16b46d
ayman-gassi <itsmeaymane2003@gmail.com>
2023-12-31 18:21:54 +0100

    initialize project

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './Assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          dd
+      </header>
+    </div>
+  );
+}
+
+export default App;
